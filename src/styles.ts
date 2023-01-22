9af1f608898d66d8946fbcdf600d40348e51fefb
Daiki48 <daiki48.engineer@gmail.com>
2023-01-23 00:07:32 +0900

    :wrench: Position center

--- src/styles.ts
+++ src/styles.ts
@@ -1,6 +1,9 @@
-export const imgDisplaySize = { width: 200, height: 200 };
+export const imgDisplaySize = { width: 600, height: 400 };
 export const styles = {
   imgContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     width: imgDisplaySize.width,
     height: imgDisplaySize.height,
     marginTop: 10,
